1f5ac6397005016d41e69b7b9d9b305a38882850
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2023-01-08 21:38:00 -0400

    change: status active

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,22 +1,30 @@
+const div = document.querySelectorAll("main > div");
+const span = document.querySelector("main > div > span");
+const getVote = document.querySelectorAll("div.star > ul > li");
+const getSubmit = document.querySelector("div.star > button");
 let vote = 0;
+
 function voting(i){
     vote = i.target.id;
+    getVote.forEach(value => {
+        if(value.id == vote){
+            value.classList.add("active");
+        }else{
+            value.classList.remove("active");
+        }
+    });
 }
 function submit(){
     if(vote){
-        const span = document.querySelector("main > div > span");
-        span.textContent = `You selected ${vote} out of 5`
-        const div = document.querySelectorAll("main > div");
-        div.forEach(value=>{
+        span.textContent = `You selected ${vote} out of 5`;
+        div.forEach(value => {
             value.classList.toggle("hidden");
         });
     }
 }
 
-const getVote = document.querySelectorAll("div.star > ul >li");
-getVote.forEach((value)=>{
+getVote.forEach((value) => {
     value.addEventListener("click", voting);
 });
 
-const getSubmit = document.querySelector("div.star > button");
 getSubmit.addEventListener("click", submit);
