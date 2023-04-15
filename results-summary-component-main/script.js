190e22030a82f6385677c4bcd39031cb364eb01d
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2023-04-15 18:04:58 -0400

    change: inserta score dinamico desde JSON

---
+++ results-summary-component-main/script.js
@@ -0,0 +1,19 @@
+(async ()=>{
+    const data = await fetch("./data.json",{cache:"no-cache"}).then(e=>e.json());
+    const ul = document.querySelector("ul");
+    data.forEach(element => {
+        const { category, score, icon } = element;
+        const newElement = `
+            <li class="${category}">
+                <div class="nameData">
+                    <img src=${icon}>
+                    <h4>${category}</h4>
+                </div>
+                <div class="data">
+                    <p>${score}<span> / 100</span></p>
+                </div>
+            </li>
+        `;
+        ul.insertAdjacentHTML("beforeend",newElement);
+    });
+})();
