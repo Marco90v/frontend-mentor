2a0fc7a5e9d4423a4633f0a8c08abb857c5ea2b7
i320 <i320@i320.debian>
2023-07-11 03:57:10 -0400

    add: mobil design completed

---
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,31 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const paramEmail = urlParams.get('email');
+const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+paramEmail ? submitted() : confForm();
+
+function submitted(){
+    const spanEmail = document.getElementById("email");
+    spanEmail.innerText = `${paramEmail}.`;
+}
+
+function confForm(){
+    const formSend = document.getElementById("formSend");
+    formSend.addEventListener("submit", send);
+}
+
+function send(e){
+    e.preventDefault();
+    const spanError = e.target.children['error'];
+    const email = e.target["email"];
+    const validate = regex.test(email.value);
+    if(!validate){
+        spanError.classList.add('error');
+        email.classList.add('error');
+    }else{
+        spanError.classList.remove('error');
+        email.classList.remove('error');
+        location.href = `./send.html?email=${email.value}`;
+    }
+}
