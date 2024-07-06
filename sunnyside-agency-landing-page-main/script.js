1f6bfa0717360786b235484e03af6b971d8d0069
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-07-06 18:38:04 -0400

    vista desktop completada

--- sunnyside-agency-landing-page-main/script.js
+++ sunnyside-agency-landing-page-main/script.js
@@ -2,8 +2,18 @@ const btnMenu = document.querySelector(".btn-menu")
 const menu = document.querySelector(".menu")
 
 function toogleMenu(){
-    // console.log(menu)
     menu.classList.toggle("show")
 }
 
+function removeClass(c) {
+    menu.classList.remove(c)
+}
+
+function resize(e) {
+    const width = e.target.innerWidth;
+    if (width > 765) removeClass("show")
+}
+
 btnMenu.addEventListener("click", toogleMenu)
+
+window.addEventListener("resize", resize)
