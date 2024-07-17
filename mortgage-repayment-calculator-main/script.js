10a4896681b42832917ed6c0aa5a3679a32e2681
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-07-16 22:05:20 -0400

    feat: completado el script y vista desktop

--- mortgage-repayment-calculator-main/script.js
+++ mortgage-repayment-calculator-main/script.js
@@ -1,24 +1,48 @@
 const form = document.querySelector("form");
+const clear = document.querySelector(".clear");
 
 function showResult(monthlyPayment, totalRepayment) {
     const preResult = document.querySelector(".preResult");
     const postResult = document.querySelector(".postResult");
     const monthlyPay = document.querySelector(".monthly > .pay");
     const totalPay = document.querySelector(".total > .pay");
-    
-    preResult.classList.add("hidden");
-    postResult.classList.remove("hidden");
-    monthlyPay.innerHTML = `${"£" + monthlyPayment.toString()}`;
-    totalPay.innerHTML = `${"£" + totalRepayment.toString()}`;
+    if(monthlyPayment && totalRepayment){
+        preResult.classList.add("hidden");
+        postResult.classList.remove("hidden");
+        monthlyPay.innerHTML = `£${monthlyPayment.toString()}`;
+        totalPay.innerHTML = `£${totalRepayment.toString()}`;
+    }else{
+        preResult.classList.remove("hidden");
+        postResult.classList.add("hidden");
+        monthlyPay.innerHTML = "";
+        totalPay.innerHTML = "";
+    }
 }
 
 function validateInput(obj) {
-    const {amount,rate,term} = obj;
-    if(amount!="" && term!="" && rate!=""){
-        return typeof amount === "number" && typeof term === "number" && typeof rate === "number";        
+    let vI = true;
+    const {type, ...input} = obj;
+    Object.entries(input).forEach(([k,v])=>{
+        const elements = document.querySelector(`#${k}`);
+        if(v === 0){
+            vI = false;
+            elements.classList.add("error");
+            elements.parentNode.nextSibling.nextSibling.innerHTML = "This field is required";
+            elements.parentNode.nextSibling.nextSibling.classList.add("mt");
+        }else{
+            elements.classList.remove("error");
+            elements.parentNode.nextSibling.nextSibling.innerHTML = "";
+            elements.parentNode.nextSibling.nextSibling.classList.remove("mt");
+        }
+    })
+    const elements = document.querySelector("#typeError");
+    if(!type){
+        vI = false;
+        elements.innerHTML = "This field is required";
     }else{
-        return false
+        elements.innerHTML = "";
     }
+    return vI;
 }
 
 function handlerSubmit(e) {
@@ -26,7 +50,6 @@ function handlerSubmit(e) {
     const data = {}
     let monthlyPayment = 0;
     let totalRepayment = 0;
-
     for (let index = 0; index < e.target.length; index++) {
         if(e.target[index].type === "number"){
             data[e.target[index].id] = Number(e.target[index].value);
@@ -48,6 +71,18 @@ function handlerSubmit(e) {
         }
         showResult(monthlyPayment.toFixed(2), totalRepayment.toFixed(2));
     }
+    else { showResult(0, 0); }
+}
+
+function handlerClick() {
+    for (let index = 0; index < form.elements.length; index++) {
+        if(form.elements[index].type === "number"){
+            form.elements[index].value = "";
+        }else if(form.elements[index].type === "radio"){
+            form.elements[index].checked = false;
+        }
+    }
 }
 
 form.addEventListener("submit", handlerSubmit);
+clear.addEventListener("click", handlerClick);
